8cc09b152161fb11aa1bfa2e1200e4c174f2346a
AlexSoftware31 <inf.2231@gmail.com>
2025-11-03 03:28:25 -0400

    added use prop

--- src/app/princing/page.tsx
+++ src/app/princing/page.tsx
@@ -1,18 +1,14 @@
-"use client";
-
 import Pricing from "@/components/Pricing";
 import { Metadata } from "next";
-import { useSearchParams } from "next/navigation";
 
-// export const metadata: Metadata = {
-//   title: "Harmony & Symphony",
-//   description: "Precios",
-//   // other metadata
-// };
+export const metadata: Metadata = {
+  title: "Harmony & Symphony",
+  description: "Precios",
+  // other metadata
+};
 
-const PricesPage = () => {
-  const searchParams = useSearchParams();
-  const email = searchParams.get("email");
+const PricesPage = ({ searchParams }: { searchParams: { email?: string } }) => {
+  const email = searchParams.email;
 
   return (
     <>
