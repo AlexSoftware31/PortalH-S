0cc976583a1946b4c37a35ad3dd73e5def1eb780
AlexSoftware31 <inf.2231@gmail.com>
2025-11-15 14:47:25 -0400

    Updated labels

--- src/app/blog-sidebar/page.tsx
+++ src/app/blog-sidebar/page.tsx
@@ -459,7 +459,7 @@ const BlogSidebarPage = () => {
                 </div>
               </div>
 
-              <NewsLatterBox />
+              {/* <NewsLatterBox /> */}
             </div>
           </div>
         </div>
